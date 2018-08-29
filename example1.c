2d4490291dcad7bcab118db9ded677cd8fed0821
ShenDeZhou(搜索事业部_复合搜索应用研发部) <shendezhou@sogou-inc.com>
2018-08-29 20:10:58 +0800

    fix format.

--- example1.c
+++ example1.c
@@ -4,30 +4,27 @@
 #include "kmeans.h"
 
 void kmeans(
-            int  dim,		                     // dimension of data
-
-            double *X,                        // pointer to data
-            int   n,                         // number of elements
-
-            int   k,                         // number of clusters
-            double *cluster_centroid,         // initial cluster centroids
-            int   *cluster_assignment_final  // output
-           );
-int main(int nargs, char **args) {
-	double v[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-	double c[2] = {2.0, 5.0};
-  int r[10];
-  
-  kmeans(1,
-    v,
-    10,
-    2,
-    c,
-    r
+  int dim,					// dimension of data
+  double *X,					// pointer to data
+  int n,					// number of elements
+  int k,					// number of clusters
+  double *cluster_centroid,			// initial cluster centroids
+  int   *cluster_assignment_final		// output
   );
 
+int main(int nargs, char **args) {
+  double v[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+  double c[2] = {2.0, 5.0};
+  int r[10];
+  kmeans(1,
+         v,
+         10,
+         2,
+         c,
+         r
+         );
   printf("cluster result:");
   for (int ii = 0; ii < 10; ii++)
     printf("%d,", r[ii]);
-	return 0;
+  return 0;
 }
--- kmeans.h
+++ kmeans.h
@@ -3,12 +3,10 @@
 // October 2011
 
 void kmeans(
-            int  dim,		                     // dimension of data 
-
-            double *X,                        // pointer to data
-            int   n,                         // number of elements
-            
-            int   k,                         // number of clusters
-            double *cluster_centroid,         // initial cluster centroids
-            int   *cluster_assignment_final  // output
-           );
+  int dim,						// dimension of data
+  double *X,					// pointer to data
+  int n,					// number of elements
+  int k,					// number of clusters
+  double *cluster_centroid,			// initial cluster centroids
+  int   *cluster_assignment_final		// output
+  );
